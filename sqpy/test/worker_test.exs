ab6eff4d94fe72a74fbd3bae66ef8d15df480bba
rachit-ranjan16 <rachit.ranjan93@gmail.com>
2018-09-10 15:53:10 -0400

    Refactored into logical units Fixed Failing Test Cases

--- sqpy/test/worker_test.exs
+++ sqpy/test/worker_test.exs
@@ -1,26 +1,26 @@
-defmodule SqPyTest do
+defmodule WorkerTest do
   use ExUnit.Case
-  doctest SqPy
+  doctest Worker
 
   test "check if 25 is square or not" do
-    assert SqPy.is_square?(25) === true
+    assert Worker.is_square?(25) === true
   end
 
   test "check if 24 is square or not" do
-    assert SqPy.is_square?(24) === false
+    assert Worker.is_square?(24) === false
   end
 
   test "square sum logic incorrect args" do
-    assert SqPy.square_sum(5,3) === 0
+    assert Worker.square_sum(5,3) === 0
   end 
 
   # 3*3 + 4*4 + 5*5 = 50
   test "square sum logic correct args" do
-    assert SqPy.square_sum(3,5) === 50
+    assert Worker.square_sum(3,5) === 50
   end
 
   test "square sum neagtive args" do
-    assert SqPy.square_sum(-6,-5) === 0
+    assert Worker.square_sum(-6,-5) === 0
   end
 
   # test "lucas square for valid args" do
